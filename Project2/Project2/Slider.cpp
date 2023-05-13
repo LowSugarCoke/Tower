910aefeb3ec40d654b227fbf3834a10d536f5b82
Jack Lee <jack20499@gmail.com>
2023-05-13 23:12:48 +0800

    complete slider

--- Project2/Project2/Slider.cpp
+++ Project2/Project2/Slider.cpp
@@ -4,6 +4,9 @@
 #include "Point.hpp"
 #include "Slider.hpp"
 
+#include "LOG.hpp"
+#include "GameEngine.hpp"
+
 Slider::Slider(float x, float y, float w, float h) :
 	ImageButton("stage-select/slider.png", "stage-select/slider-blue.png", x, y),
 	Bar("stage-select/bar.png", x, y, w, h),
@@ -25,21 +28,37 @@ void Slider::SetOnValueChangedCallback(std::function<void(float value)> onValueC
 void Slider::SetValue(float value) {
 	// TODO 1 (4/7): Call 'OnValueChangedCallback' when value changed. Can imitate ImageButton's 'OnClickCallback'.
 	// Also, move the slider along the bar, to the corresponding position.
-	if (this->value != value) {
-		this->value = value;
-		if (OnValueChangedCallback) {
-			OnValueChangedCallback(this->value);
-		}
-		Position.x = Bar.Position.x + value * Bar.GetBitmapWidth();
+	if (value < Min) value = Min;
+	else if (value > Max) value = Max;
+	this->value = value;
+	Position.x = Bar.Position.x + (Bar.Size.x * this->value);
+	
+	if (OnValueChangedCallback) {
+		OnValueChangedCallback(this->value);
 	}
+
 }
 void Slider::OnMouseDown(int button, int mx, int my) {
 	// TODO 1 (5/7): Set 'Down' to true if mouse is in the slider.
+	if (button == 1) {
+		if (mx >= Position.x - Size.x / 2 && mx <= Position.x + Size.x / 2 &&
+			my >= Position.y - Size.y / 2 && my <= Position.y + Size.y / 2) {
+			Down = true;
+		}
+	}
 
 }
 void Slider::OnMouseUp(int button, int mx, int my) {
 	// TODO 1 (6/7): Set 'Down' to false.
+	if (button == 1) {
+		Down = false;
+	}
 }
 void Slider::OnMouseMove(int mx, int my) {
+
 	// TODO 1 (7/7): Clamp the coordinates and calculate the value. Call 'SetValue' when you're done.
+	if (Down) {
+		float newValue = (mx - Bar.Position.x) / Bar.Size.x;
+		SetValue(newValue);
+	}
 }
